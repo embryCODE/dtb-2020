f1e63f676a7e3d10a64b7d04f5ca5f5a8a37e660
Mason Embry <mason@embrycode.com>
2020-01-26 17:32:19 -0600

    Design tweaks on home page

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -15,9 +15,6 @@ const IconLink: React.FC<IconLinkProps> = ({ href, ...otherProps }) => {
           width: 100,
           height: 'auto',
           cursor: 'pointer',
-          filter: 'grayscale(90%)',
-          opacity: 0.9,
-          '&:hover': { filter: 'grayscale(0)', opacity: 1 },
         }}
       />
     </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import {
   Link,
   Text,
   Flex,
+  Card,
 } from '@theme-ui/components'
 import { Link as RouterLink } from 'gatsby'
 import { APP_MAX_WIDTH } from '../config'
@@ -35,63 +36,59 @@ const Home = () => (
         maxWidth: APP_MAX_WIDTH,
       }}
     >
-      <Grid mt={3} columns={[1, 2]} gap={4}>
-        <Box>
-          <Text sx={{ fontSize: 4 }}>
-            Voted{' '}
-            <Box as="span" color="primary">
-              &ldquo;Best Live Band in Tennessee&rdquo;
-            </Box>{' '}
-            &{' '}
-            <Box as="span" color="primary">
-              &ldquo;Top 10 Best Live Band in United States&rdquo;
-            </Box>{' '}
-            three years in a row by The Industry Expert Awards!
-          </Text>
-        </Box>
-
-        <Flex sx={{ justifyContent: 'center', alignItems: 'center' }}>
-          <RouterLink
-            to="/contact"
-            style={{
-              color: 'inherit',
-              textDecoration: 'none',
-              maxWidth: '100%',
-            }}
-          >
-            <Button
-              as="div"
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: 80,
-                width: 350,
-                fontSize: 4,
-                maxWidth: '100%',
-              }}
-            >
-              Contact us!
-            </Button>
-          </RouterLink>
-        </Flex>
-      </Grid>
-
-      <Box mx="auto" my={5} sx={{ maxWidth: 800 }}>
+      <Box mx="auto" mt={4} mb={5} sx={{ maxWidth: 800 }}>
         <Videos links={['https://www.youtube.com/embed/I6hmxc4fGXk']} />
       </Box>
 
-      <Grid mt={4} columns={[1, 2]}>
-        <Text as="p">
-          The Downtown Band is a high-energy, dance-driven cover band that
-          delivers top quality live entertainment. This 5 to 10-piece group can
-          play everything from your full blown after party to an elegant
-          cocktail hour.
-        </Text>
+      <Flex sx={{ justifyContent: 'center', alignItems: 'center' }}>
+        <RouterLink
+          to="/contact"
+          style={{
+            color: 'inherit',
+            textDecoration: 'none',
+            maxWidth: '100%',
+          }}
+        >
+          <Button
+            as="div"
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 100,
+              width: 400,
+              fontSize: 4,
+              maxWidth: '100%',
+            }}
+          >
+            Contact us!
+          </Button>
+        </RouterLink>
+      </Flex>
 
-        <Box as="p">
-          Performed for <Link href="http://www.eltonjohn.com/">Elton John</Link>
-          , <Link href="http://www.sting.com/">Sting</Link>,{' '}
+      <Text
+        mt={5}
+        mx="auto"
+        sx={{ maxWidth: 800, fontSize: 4, textAlign: 'center' }}
+      >
+        Voted{' '}
+        <Box as="span" color="primary">
+          &ldquo;Best Live Band in Tennessee&rdquo;
+        </Box>{' '}
+        &{' '}
+        <Box as="span" color="primary">
+          &ldquo;Top 10 Best Live Band in United States&rdquo;
+        </Box>{' '}
+        three years in a row by The Industry Expert Awards!
+      </Text>
+
+      <Grid mt={5} columns={[1, 2]} gap={4}>
+        <Card bg="black" p={4} sx={{ fontSize: 4, textTransform: 'uppercase' }}>
+          <Heading mb={3} sx={{ textAlign: 'center' }}>
+            Performed for
+          </Heading>{' '}
+          <Link href="http://www.eltonjohn.com/">Elton John</Link>,{' '}
+          <Link href="http://www.sting.com/">Sting</Link>,{' '}
           <Link href="https://en.wikipedia.org/wiki/David_Letterman#External_links">
             David Letterman
           </Link>
@@ -103,51 +100,55 @@ const Home = () => (
           <Link href="https://timmcgraw.com/">Tim McGraw</Link>,{' '}
           <Link href="https://www.rascalflatts.com/">Rascal Flatts</Link>, and
           YOU!
-        </Box>
+        </Card>
+
+        <Card bg="black" p={4}>
+          <Heading
+            sx={{
+              textAlign: 'center',
+              fontSize: '36px',
+              textTransform: 'uppercase',
+            }}
+          >
+            Corporate Events
+          </Heading>
+
+          <Grid
+            mt={4}
+            columns={[3, 4]}
+            gap={[3, 4, 5]}
+            sx={{ alignItems: 'center' }}
+          >
+            <IconLink
+              alt="Southwest Airlines icon"
+              src={southwest}
+              href="http://www.southwest.com/"
+            />
+            <IconLink
+              alt="Frito Lay icon"
+              src={frito}
+              href="http://www.fritolay.com/"
+            />
+            <IconLink
+              alt="Coca Cola icon"
+              src={coke}
+              href="http://coca-cola.com/"
+            />
+            <IconLink alt="Ford icon" src={ford} href="http://www.ford.com/" />
+            <IconLink
+              alt="Microsoft icon"
+              src={microsoft}
+              href="https://www.microsoft.com/"
+            />
+            <IconLink alt="NBC icon" src={nbc} href="http://www.nbc.com/" />
+            <IconLink
+              alt="Ritz-Carlton icon"
+              src={ritz}
+              href="http://www.ritzcarlton.com/"
+            />
+          </Grid>
+        </Card>
       </Grid>
-
-      <Box mt={5} as="hr" />
-
-      <Box mt={4}>
-        <Heading color="primary" sx={{ textAlign: 'center', fontSize: 5 }}>
-          Corporate Events
-        </Heading>
-
-        <Grid
-          mt={4}
-          columns={[4, 4, 7]}
-          gap={[3, 4, 5]}
-          sx={{ alignItems: 'center' }}
-        >
-          <IconLink
-            alt="Southwest Airlines icon"
-            src={southwest}
-            href="http://www.southwest.com/"
-          />
-          <IconLink
-            alt="Frito Lay icon"
-            src={frito}
-            href="http://www.fritolay.com/"
-          />
-          <IconLink
-            alt="Coca Cola icon"
-            src={coke}
-            href="http://coca-cola.com/"
-          />
-          <IconLink alt="Ford icon" src={ford} href="http://www.ford.com/" />
-          <IconLink
-            alt="Microsoft icon"
-            src={microsoft}
-            href="https://www.microsoft.com/"
-          />
-          <IconLink alt="NBC icon" src={nbc} href="http://www.nbc.com/" />
-          <IconLink
-            alt="Ritz-Carlton icon"
-            src={ritz}
-            href="http://www.ritzcarlton.com/"
-          />
-        </Grid>
-      </Box>
     </Box>
   </Layout>
 )
